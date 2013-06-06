7451da975b56f1a3446ea9e648f9eacaabeb0ff9
Thomas A Forsberg <taforsberg89@gmail.com>
2013-06-06 18:13:58 +0200

    Removed return type from Refine method

--- Web_Application/SearchSystem/Refiner.cs
+++ Web_Application/SearchSystem/Refiner.cs
@@ -9,6 +9,8 @@ namespace SearchSystem
     public class Refiner
     {
 
+        //This class is used for refining the results after receiving them from Bing
+
         List<Context> contexts = new List<Context>();
 
         public Refiner(List<Context> contexts)
@@ -16,59 +18,59 @@ namespace SearchSystem
             this.contexts = contexts;
         }
 
-        public List<Result> Refine(List<Result> results)
-	    {
-	
-		    List<Result> refinedResults = new List<Result>();
-		
-		    for(int i = 0; i < results.Count; i++)
-		    {
-			    for(int y = 0; y < contexts.Count; y++)
-			    {
+        public void Refine(List<Result> results)
+        {
+
+
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                for (int y = 0; y < contexts.Count; y++)
+                {
                     for (int k = 0; k < contexts[y].tags.Count; k++)
-				    {
-					    if(results[i].url.Contains(contexts[y].tags[k]))
-					    {
-						    results[i].AddRelevance();
-					    }
+                    {
+                        if (results[i].url.Contains(contexts[y].tags[k]))
+                        {
+                            results[i].AddRelevance();
+                        }
                         if (results[i].description.Contains(contexts[y].tags[k]))
-					    {
-						    results[i].AddRelevance();
-					    }
-					    if(results[i].title.Contains(contexts[y].tags[k]))
-					    {
-						    results[i].AddRelevance();
-					    }
-				    }
-			    }
-			
-		    }
-		
-		    foreach(Result r in results)
-		    {
-			    if(r.GetRelevance() > 0)
-			    {
-				
-				    refinedResults.Add(r);
-				
-			    }
-		    }
-		
-		    for (int i = 0; i < (refinedResults.Count - 1); i++)
-		    {
-			    for (int j = (i + 1); j < refinedResults.Count; j++) 
-			    {
-				    if (refinedResults[i].GetRelevance() > refinedResults[j].GetRelevance())
-				    {
-					    Result temp = refinedResults[i];
-					    refinedResults[i] = refinedResults[j];
-					    refinedResults[j] = temp;
-				    }
-			    }
-		    }
-		
-		    return refinedResults;
-	}
+                        {
+                            results[i].AddRelevance();
+                        }
+                        if (results[i].title.Contains(contexts[y].tags[k]))
+                        {
+                            results[i].AddRelevance();
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i].GetRelevance() == 0)
+                {
+
+                    results.Remove(results[i]);
+
+                }
+            }
+
+            for (int i = 0; i < (results.Count - 1); i++)
+            {
+                for (int j = (i + 1); j < results.Count; j++)
+                {
+                    if (results[i].GetRelevance() > results[j].GetRelevance())
+                    {
+                        Result temp = results[i];
+                        results[i] = results[j];
+                        results[j] = temp;
+                    }
+                }
+            }
+
+
+        }
 
 
     }
